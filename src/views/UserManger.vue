ef0a6a7bb0d30b53fc12b268668fb12b84fab5e7
Kevin Barnett <kevin@shapley.io>
2024-04-10 21:14:12 -0700

    start implementing some views and kustomize

---
+++ src/views/UserManger.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="user-management">
+      <UserManagment />
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { Options, Vue } from 'vue-class-component';
+  import UserManagment from '@/components/UserManagment.vue'; // @ is an alias to /src
+  
+  @Options({
+    components: {
+      UserManagment,
+    },
+  })
+  export default class Login extends Vue { }
+  </script>
+  
